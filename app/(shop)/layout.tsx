7f86374d60752221cfc9b0c0e4a91bc5c4ccaf28
michael-grier <michael.c.grier@gmail.com>
2026-07-29 09:51:01 -0600

    style: update nav bar to fit with redesigned landing page

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -12,7 +12,8 @@ export default function ShopLayout({
   return (
     <CartSidebar>
       <SiteHeader />
-      {children}
+      {/* Offsets the fixed floating header; full-bleed heroes pull under it with -mt-[68px]. */}
+      <div className="pt-[68px]">{children}</div>
       <SiteFooter />
     </CartSidebar>
   );
