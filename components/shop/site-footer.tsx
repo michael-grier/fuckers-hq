b748bc2b000dbfbeee70e4b835773f996ac5b04c
Michael Grier <michael.c.grier@gmail.com>
2026-07-30 01:54:14 -0600

    style: reduce footer height

--- components/shop/site-footer.tsx
+++ components/shop/site-footer.tsx
@@ -12,7 +12,7 @@ const socialLinks: ReadonlyArray<{
 export function SiteFooter() {
   return (
     <footer className="border-t bg-surface-chrome text-white">
-      <div className="flex flex-col gap-3 px-6 py-8 md:flex-row md:items-center md:justify-between lg:px-8">
+      <div className="flex flex-col gap-2 px-6 py-4 md:flex-row md:items-center md:justify-between lg:px-8">
         <p className="font-bold font-grotesk text-xl tracking-tight">
           Fuckers <span className="text-accent">Skateboards</span>
         </p>
